fdd73476d3274226977cbe14af89096f0d17b9a3
Wilfred <wilfredkim5@gmail.com>
2023-05-07 22:47:39 +0300

    initial commit of microservices spring boot 3.0 +

---
+++ src/main/java/com/wilfred/microservices/microservicespringboot3_0/MicroserviceSpringBoot30Application.java
@@ -0,0 +1,13 @@
+package com.wilfred.microservices.microservicespringboot3_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringBoot30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSpringBoot30Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilfred/microservices/microservicespringboot3_0/MicroserviceSpringBoot30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilfred.microservices.microservicespringboot3_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringBoot30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
